05ac361ed3c7c2826f5f4fb60cc38fa2884c1cd5
ernesto_espinosa@epam.com <ernesto_espinosa@epam.com>
2016-09-08 18:47:42 -0500

    Changed structure to match with parameterized test

--- src/mx/epam/algorithms/lib/BaseTest.java
+++ src/mx/epam/algorithms/lib/BaseTest.java
@@ -7,39 +7,45 @@ import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Base test class for algorithm testing.
  * @author Ernesto Espinosa.
  */
-public abstract class BaseTest<I,O> implements Runnable {
+public abstract class BaseTest {
+	
+	protected List<Path> inputs;
+	protected List<Path> outputs;
 
-	public void run() {
-		System.out.println(IOUtils.getClassFolderPath(this.getClass()));
-		
+	/**
+	 * Initializer that gets the list of inputs and outputs.
+	 */
+	public BaseTest() {
 		try (DirectoryStream<Path> stream = Files
 				.newDirectoryStream(IOUtils.getClassFolderPath(this.getClass()), "*.in")) {
 			Path output;
 			String filename;
 			
+			this.inputs = new ArrayList<>();
+			this.outputs = new ArrayList<>();
+			
 			for (Path entry : stream) {
+				inputs.add(entry);
 				filename = entry.getName(entry.getNameCount()-1).toString();
 				output = entry.resolveSibling(filename.substring(0, filename.indexOf('.')) + ".out");
-				
-				System.out.println(output);
+				outputs.add(output);
 			}
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	public abstract O parseAndExecute();
-	
-	public static void main(String...args) {
-		new BaseTest<Object,Object>() {
-			public Object parseAndExecute() {
-				return null;
-			}
-		}.run();
-	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public abstract Collection<Object[]> getTestData();
 }
