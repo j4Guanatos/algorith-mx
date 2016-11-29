f2226efe28f494c298304a30c1a990e384a98337
Sier Violencia <sierisimo@gmail.com>
2016-11-29 15:29:52 -0600

    Fixed to O(n) the BigO

--- 00_BigO/src/BigO_00.kt
+++ 00_BigO/src/BigO_00.kt
@@ -2,34 +2,26 @@
  * Created by Sinuhe_Jaime on 25/11/2016.
  */
 fun solution1(arrayOfInts: Array<Int>, number: Int): Set<Pair<Int, Int>> {
-    // This tooks O(n) in the worst case which is when the array is almost sorted.
-    arrayOfInts.sort()
-
     // Filter elements of the sorted array to only the ones that are less than the number
-    // Filter could be improved if we found the index of the element that is less than the number
     val listOfPossible = arrayOfInts.filter { it <= number }
+            .toMutableList()
 
-    val pairs = mutableSetOf<Pair<Int, Int>>()
-
-    var idxFirst = 0
-    var idxLast = listOfPossible.size - 1
-
-    while (idxFirst != idxLast) {
-        val first = listOfPossible[idxFirst]
-        val last = listOfPossible[idxLast]
-
-        val sum = first + last
-        if (sum > number) {
-            idxLast--
-        } else if (sum < number) {
-            idxFirst++
-        } else {
-            pairs.add(Pair(listOfPossible[idxFirst], listOfPossible[idxLast]))
-            idxFirst++
+    val resultSet = listOfPossible.fold(mutableSetOf<Pair<Int, Int>>(), { mSet, element ->
+        val complement = number - element
+        if (listOfPossible.contains(complement)) {
+            when (complement > element) {
+                true -> mSet.add(Pair(element, complement))
+                false -> mSet.add(Pair(complement, element))
+            }
         }
-    }
 
-    return pairs
+        mSet
+    })
+
+    println("Set")
+    println(resultSet)
+
+    return resultSet
 }
 
 fun main(args: Array<String>) {
