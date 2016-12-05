9d17fc5bac3f77d821a82dd0ee695e81aa7b0a75
Oscar Legorreta <oscar_legorreta@epam.com>
2016-12-05 10:55:16 -0600

    Two lines, so sad

--- 01_Arrays/Arrays.cs
+++ 01_Arrays/Arrays.cs
@@ -1,14 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-public class Arrays
-{
+
+public class Arrays{
     public static int Problem1(IEnumerable<int> array)
     {
         return ((array.Count()+1) * (array.Count() + 2) / 2) - array.Sum(); //O(N)
     }
-    public static IEnumerable<int> Problem2(IEnumerable<int> array, int j, int k)
+    public static Tuple<int,int> Problem2(IEnumerable<int> array,int j, int k)
     {
-        return array.OrderBy(n => n).Where( (v,i) => i == (j - 1) || i == array.Count()-k ); // O(N log N)
+        var sorted = array.OrderBy(n => n); // O(N log N)
+        return new Tuple<int, int>(sorted.ElementAt(j-1) , sorted.ElementAt(sorted.Count()-k)); // O(N)
     }
 }
