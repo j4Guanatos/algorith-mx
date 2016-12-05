acd41a482c97b2d46d79635095146665b62ef4df
Oscar Legorreta <oscar_legorreta@epam.com>
2016-12-05 14:09:01 -0600

    Two lines, so sad

--- 01_Arrays/Arrays.cs
+++ 01_Arrays/Arrays.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class Arrays{
-    public static int Problem1(IEnumerable<int> array)
+    public static int Problem1(int[] array)
     {
-		var n = array.Count()+1;
+		var n = array.Length+1; // O(1)
         return (n * (n+1) / 2) - array.Sum(); //O(N)
     }
-    public static Tuple<int,int> Problem2(IEnumerable<int> array,int j, int k)
+    public static Tuple<int,int> Problem2(int[] array,int j, int k)
     {
-        var sorted = array.OrderBy(n => n); // O(N log N)
-        return new Tuple<int, int>(sorted.ElementAt(j-1) , sorted.ElementAt(sorted.Count()-k)); // O(N)
+        var sorted = array.OrderBy(n => n).ToArray(); // O(N log N)
+        return new Tuple<int, int>(sorted[j-1] , sorted[sorted.Length-k]); // O(1)
     }
 	public static void Main(string[] args){
 		int[] input1 = new int[]{9,3,7,2,6,8,4,5,10};
@@ -22,6 +22,6 @@ public class Arrays{
 		int j = 2;
 		int k = 4;
 		var result = Problem2(input2,j,k);
-		Console.WriteLine($"For input {joined}, j={j}, k={k}, the jth smallest is:{result.Item1} and the kth largest is: {result.Item2}");
+		Console.WriteLine($"For input [{joined}], j={j}, k={k}, the jth smallest is:{result.Item1} and the kth largest is: {result.Item2}");
 	}
 }
