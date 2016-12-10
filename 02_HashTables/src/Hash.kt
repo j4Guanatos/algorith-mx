7e3a762a594e80ab63b3a6dedd5aaec28caa58cd
Sier Violencia <sierisimo@gmail.com>
2016-12-09 19:06:27 -0600

    Star4ting algortithmasd

---
+++ 02_HashTables/src/Hash.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+/**
+ * Created by Sinuhe_Jaime on 09/12/2016.
+ */
+
+fun searchPairs(array: Array<Int>, target: Int) {
+    val hashTable: Hashtable<Int, Pair<Int, Int>> = Hashtable()
+
+
+}
+
+fun main(args: Array<String>) {
+
+}
