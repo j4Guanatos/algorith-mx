a7348b6fe209c9912978ae9708ac29f8dbe2e5ff
Sier Violencia <sierisimo@gmail.com>
2016-12-12 16:27:57 -0600

    Sorry, bad commit

--- 02_HashTables/src/Sier.kt
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Sinuhe_Jaime on 12/12/2016.
- */
-
-class Sier(val name: String, val age: Int) {
-    override fun hashCode(): Int {
-        return name.hashCode() + age.hashCode()
-    }
-}
