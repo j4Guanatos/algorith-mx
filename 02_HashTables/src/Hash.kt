7b8bae6fe19a2548cc96466c79c89fc46658625c
Sier Violencia <sierisimo@gmail.com>
2016-12-12 16:26:25 -0600

    HashTable re-do

--- 02_HashTables/src/Hash.kt
+++ 02_HashTables/src/Hash.kt
@@ -4,12 +4,28 @@ import java.util.*
  * Created by Sinuhe_Jaime on 09/12/2016.
  */
 
-fun searchPairs(array: Array<Int>, target: Int) {
-    val hashTable: Hashtable<Int, Pair<Int, Int>> = Hashtable()
+fun searchPairs(array: Array<Int>, target: Int): Pair<Int, Int> {
+    val hashTable: Hashtable<Int, Int> = Hashtable()
 
+    var complement: Int
+    for ((i, v) in array.withIndex()) {
+        if (hashTable[v] != null) {
+            return Pair(hashTable[v]!!, i)
+        } else {
+            complement = target - v
 
+            hashTable[complement] = i
+        }
+    }
+
+    return Pair(-1, -1)
 }
 
 fun main(args: Array<String>) {
+    var arr = arrayOf(2, 7, 11, 15)
+    var target = 8
 
+    arr = arrayOf(3, 7, 9, 0, 20, 30, 1000000, 1000, 900000, 90, 5, 10)
+
+    println(searchPairs(arr, target))
 }
