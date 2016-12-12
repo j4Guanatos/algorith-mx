6c6679a6bbb71976f0f00277e43cb0b607c96aad
enchf <e.ernesto.espinosa@gmail.com>
2016-12-12 03:56:39 -0600

    Added test case creator for sum pair indexes problem

--- 02_HashTables/j4g/SumPairIndex.java
+++ 02_HashTables/j4g/SumPairIndex.java
@@ -2,7 +2,7 @@ package j4g;
 
 import j4g.util.IntTuple;
 
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -38,12 +38,62 @@ public class SumPairIndex {
         return IntTuple.get(sumEntry.getValue(), hash.get(target - sumEntry.getKey()));
     }
 
+    public class TestCase {
+        int size;
+        int firstPlaceholder = 0;
+        int secondPlaceholder = 1;
+        int targetDiff = 10;
+
+        private TestCase(int size) {
+            this.size = size;
+        }
+
+        public TestCase size(int size) {
+            this.size = size;
+            return this;
+        }
+
+        public TestCase targetDiff(int diff) {
+            this.targetDiff = Math.max(10, diff);
+            return this;
+        }
+
+        public TestCase firstPlaceholder(int i) {
+            if (i == secondPlaceholder) throw new IllegalArgumentException();
+            firstPlaceholder = i;
+            return this;
+        }
+
+        public TestCase secondPlaceholder(int i) {
+            if (i == firstPlaceholder) throw new IllegalArgumentException();
+            secondPlaceholder = i;
+            return this;
+        }
+
+        public int[] create() {
+            List<Integer> randoms;
+
+            randoms = IntStream.range(target + targetDiff, target + targetDiff + size)
+                    .boxed()
+                    .collect(Collectors.toList());
+            Collections.shuffle(randoms);
+            randoms.set(firstPlaceholder, 0);
+            randoms.set(secondPlaceholder, target);
+
+            return randoms.stream().mapToInt(i -> i).toArray();
+        }
+    }
+
+    public TestCase testCaseFor(int size) {
+        return new TestCase(size);
+    }
+
     public static void main(String[] args) {
         SumPairIndex finder = SumPairIndex.find(7);
+        TestCase testCase = finder.testCaseFor(10000).firstPlaceholder(55).secondPlaceholder(999);
 
-        System.out.println(
-                finder.using(12, 4, 2, 7, 9, 27, 1, 13, 8, 16, 5, 10));
-        System.out.println(
-                finder.using(2, 3, 4, 100));
+        System.out.println(finder.using(12, 4, 2, 7, 9, 27, 1, 13, 8, 16, 5, 10));
+        System.out.println(finder.using(2, 3, 4, 100));
+        System.out.println(finder.using(testCase.create()));
     }
 }
