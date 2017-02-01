a68f0afc7aacaf659f902d6dd67fa8a51d16c6bd
Miriam Lee <miriam_lee@epam.com>
2017-02-01 16:23:08 -0600

    [10_Elementary Sorting] java sorts

---
+++ 10_Elementary Sorting/Insertion Sort.java
@@ -0,0 +1,49 @@
+public static class Insertion {// extends edu.princeton.cs.algs4.Insertion {
+
+    // is v < w ?
+
+    // is the array sorted from a[lo] to a[hi]
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+      for (int i = lo + 1; i <= hi; i++)
+        if (less(a[i], a[i - 1]))
+          return false;
+      return true;
+    }
+
+    /***************************************************************************
+     * Check if array is sorted - useful for debugging.
+     ***************************************************************************/
+    private static boolean isSorted(Comparable[] a) {
+      return isSorted(a, 0, a.length - 1);
+    }
+
+    public static String[] sort(Comparable[] a) {
+      int N = a.length;
+      String[] all = new String[a.length];
+      Arrays.fill(all, "");
+      for (int i = 0; i < N; i++) {
+        for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+          exch(a, j, j - 1);
+          showA(a, all);
+        }
+
+        assert isSorted(a, 0, i);
+      }
+      assert isSorted(a);
+      return all;
+    }
+
+  }
+
+  // print array to standard output
+  private static void show(Comparable[] a) {
+    for (int i = 0; i < a.length; i++) {
+      StdOut.println(a[i] + "  ");
+    }
+  }
+
+  private static void showA(Comparable[] a, String[] all) {
+    for (int i = 0; i < a.length; i++) {
+      all[i] += "  " + a[i];
+    }
+}
---
+++ 10_Elementary Sorting/Selection Sort.java
@@ -0,0 +1,32 @@
+public static class Selection {
+
+    public static Comparable[] sort(Comparable[] a) {
+      int N = a.length;
+      String[] all = new String[a.length];
+      Arrays.fill(all, "");
+
+      for (int i = 0; i < N; i++) {
+        int min = i;
+        for (int j = i + 1; j < N; j++) {
+          if (less(a[j], a[min]))
+            min = j;
+        }
+        exch(a, i, min);
+        showA(a, all);
+        assert isSorted(a, 0, i);
+      }
+      assert isSorted(a);
+      return all;
+    }
+
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+      for (int i = lo + 1; i <= hi; i++)
+        if (less(a[i], a[i - 1]))
+          return false;
+      return true;
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+      return isSorted(a, 0, a.length - 1);
+    }
+}
